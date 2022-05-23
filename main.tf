375f90447c858afe71314c28d5b75a638b89ec4e
Jeberson Fernando Rossetto <jeberson.fernandorossetto@thomsonreuters.com>
2022-05-23 17:13:21 -0300

    add rds module

--- main.tf
+++ main.tf
@@ -22,3 +22,12 @@ module "ec2" {
   instance_name = "ec2-teste"
   instance_type = "t3.nano"
 }
+
+module "rds" {
+  source        = "app.terraform.io/jfrossetto/rds/aws"
+  version       = "1.0.0"
+  db_name = "dbMysql"
+  db_pass = "password"
+  db_user = "admin"
+  instance_class = "db.t3.micro"
+}
