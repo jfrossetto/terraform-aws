123f2a69860f16958e25b5156b37f8c57074ac6b
Jeberson Fernando Rossetto <jeberson.fernandorossetto@thomsonreuters.com>
2022-08-25 17:43:00 -0300

    adjust to create rds in custom vpc

--- main.tf
+++ main.tf
@@ -56,28 +56,3 @@ data "aws_vpc" "selected" {
 }
 
 
-resource "aws_security_group" "allow_rds" {
-  name        = "allow_rds"
-  description = "Allow rds inbound traffic"
-  vpc_id      = data.aws_vpc.selected.id
-
-  ingress {
-    description      = "RDS from VPC"
-    from_port        = 5432
-    to_port          = 5432
-    protocol         = "tcp"
-    cidr_blocks      = [data.aws_vpc.selected.cidr_block]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_rds"
-  }
-}
-
--- rds.tf
+++ rds.tf
@@ -1,10 +1,45 @@
 module "dbdev" {
   source        = "app.terraform.io/jfrossetto/rds/aws"
-  version       = "1.0.5"
+  version       = "1.0.6"
   db_name = "dbMysqlDev"
   db_pass = "password"
   db_user = "admin"
   instance_class = "db.t3.micro"
   instance_name = "dev"
   security_rds = aws_security_group.allow_rds.id  
+  db_subnet_name = aws_db_subnet_group.db_group.id
+}
+
+resource "aws_security_group" "allow_rds" {
+  name        = "allow_rds"
+  description = "Allow rds inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+  
+  ingress {
+    description      = "RDS from VPC"
+    from_port        = 5432
+    to_port          = 5432
+    protocol         = "tcp"
+    cidr_blocks      = [var.vpc_cidr]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_rds"
+  }
+}
+
+resource "aws_db_subnet_group" "db_group" {
+  name       = "main"
+  subnet_ids = [module.vpc.private_subnets[0], module.vpc.private_subnets[1]]
+
+  tags = {
+    Name = "My DB subnet group"
+  }  
 }
